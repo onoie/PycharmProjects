082ccb1a00b1e6e3e7b999ca7a7315d3b1ad76b6
onoie <onoie3@gmail.com>
2017-09-29 12:19:09 +0900

    add kivy hello

---
+++ kivy/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding:utf-8
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class HelloApp(App):
+
+    def build(self):
+        return Label(text='Hello World')
+
+if __name__ == '__main__':
+    HelloApp().run()
+
+
